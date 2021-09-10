6d8137101e791d8594f95c77ec0d0c0d6c712082
ZhangGuoxiang <1031148430@qq.com>
2021-09-10 15:57:02 +0800

    user接口

--- src/main/java/com/zgx/blog/service/IUserService.java
+++ src/main/java/com/zgx/blog/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.zgx.blog.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zgx.blog.pojo.RespBean;
 import com.zgx.blog.pojo.User;
 
 /**
@@ -12,5 +13,25 @@ import com.zgx.blog.pojo.User;
  * @since 2021-09-10
  */
 public interface IUserService extends IService<User> {
+
+    /**
+     * 获取all用户信息
+     */
+    public RespBean getAllUser();
+
+    /**
+     * 获取用户信息
+     */
+    RespBean getUserById(Integer id);
+
+    /**
+     * 添加用户
+     */
+    RespBean addUser(User user);
+
+    /**
+     * 移除用户
+     */
+    RespBean moveUser(User user);
 
 }
--- src/main/java/com/zgx/blog/service/impl/UserServiceImpl.java
+++ src/main/java/com/zgx/blog/service/impl/UserServiceImpl.java
@@ -2,9 +2,13 @@ package com.zgx.blog.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zgx.blog.mapper.UserMapper;
+import com.zgx.blog.pojo.RespBean;
 import com.zgx.blog.pojo.User;
 import com.zgx.blog.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public RespBean getAllUser() {
+        List<User> userList = userService.list();
+        return RespBean.success("获取成功!",userList);
+    }
+
+    @Override
+    public RespBean getUserById(Integer id) {
+        User user = userService.getById(id);
+        return RespBean.success("获取成功!",user);
+    }
+
+    @Override
+    public RespBean addUser(User user) {
+        if(userService.save(user)){
+            RespBean.success("添加成功!",user);
+        }
+        return RespBean.error("添加失败!");
+    }
+
+    @Override
+    public RespBean moveUser(User user) {
+        if(userService.removeById(user)){
+            return RespBean.success("移除成功！");
+        }
+        return RespBean.error("移除失败!",user);
+    }
 }
