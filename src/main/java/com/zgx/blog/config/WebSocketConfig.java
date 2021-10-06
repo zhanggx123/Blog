35fbebd09f95a987b0b5110d0e965cba9a723028
ZhangGuoxiang <1031148430@qq.com>
2021-10-06 16:36:08 +0800

    websocket

---
+++ src/main/java/com/zgx/blog/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.zgx.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 开启WebSocket支持
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
+
--- src/main/java/com/zgx/blog/controller/OtherAPIController.java
+++ src/main/java/com/zgx/blog/controller/OtherAPIController.java
@@ -1,7 +1,9 @@
 package com.zgx.blog.controller;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.zgx.blog.service.impl.OtherApiServiceImpl;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,51 +19,16 @@ import java.util.Map;
 @RequestMapping("/OtherApi")
 public class OtherAPIController {
 
+    @Autowired
+    private OtherApiServiceImpl otherApiService;
 
     @ApiOperation(value = "新闻API")
     @GetMapping("/news")
     public String newsApi(){
-       // return "http://c.m.163.com/nc/article/headline/T1348647853363/0-40.html";
-        String url="http://c.m.163.com/nc/article/headline/T1348647853363/0-40.html";
-        String result = "";
-        BufferedReader in = null;
-        try {
-            String urlNameString = url;
-            URL realUrl = new URL(urlNameString);
-            // 打开和URL之间的连接
-            URLConnection connection = realUrl.openConnection();
-            // 设置通用的请求属性
-            connection.setRequestProperty("accept", "*/*");
-            connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 建立实际的连接
-            connection.connect();
-            // 获取所有响应头字段
-            Map<String, List<String>> map = connection.getHeaderFields();
-
-            // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream())); //这里如果出现乱码，请使用带编码的InputStreamReader构造方法，将需要的编码设置进去
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            System.out.println("发送GET请求出现异常！" + e);
-            e.printStackTrace();
-        }
-        // 使用finally块来关闭输入流
-        finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-        }
-        return result;
+       String url="http://c.m.163.com/nc/article/headline/T1348647853363/0-40.html";
+       return otherApiService.newsApi(url);
     }
+
+
 
 }
